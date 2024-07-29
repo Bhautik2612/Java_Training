51a1fa648e2fcbe282dd70cd1a17ff7203521355
Bhautik2612 <bhautikpatel2612@gmail.com>
2024-07-29 11:15:37 +0530

    Add files via upload

---
+++ Class_Work/javaexample/src/arrayafemployee/TestArrayofObject.java
@@ -0,0 +1,11 @@
+package arrayafemployee;
+
+public class TestArrayofObject {
+
+	public static void main(String[] args) {
+		
+		ArrayOfEmployee arr = new ArrayOfEmployee();
+		arr.CreateArrayOfEmp();
+	}
+
+}
